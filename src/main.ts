3513afa72988be63c4a087cfed0499c640527590
rezdelamasa <rezdelamasa@gmail.com>
2023-04-08 16:26:42 -0400

    feat: import and use uuid and click outside directives

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createPinia } from 'pinia';
+import {createPinia} from 'pinia';
 import router from '@/router/index.ts'
+import withUUID from "vue-uuid";
+import vClickOutside from "click-outside-vue3"
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
+app.use(withUUID);
 app.use(pinia);
+app.use(vClickOutside);
 app.mount('#app');
